209ab433f34b0b93f4370fdd5b589f8063e7d028
sahzad <sahjad.ranchi@gmail.com>
2020-12-27 15:20:10 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      sk
-      </div>
+    <Switch>
+      <Route exact path="/">This is home page</Route>
+      <Route exact path="/sk">This is Khans page</Route>
+      <Route>This is 404 page</Route>
+      </Switch>
+      
   );
 }
 
