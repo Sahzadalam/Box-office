c9d6006b44458d71c3ffabc15cb8e243e1ee763b
sahzad <sahjad.ranchi@gmail.com>
2020-12-28 00:12:31 +0530

    showed fetched results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,12 @@ import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
   const [input, setInput] = useState('');
-
+const [results, setResults] = useState(null);
   const onSearch = () => {
     fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
       .then(r => r.json())
       .then(result => {
+          setResults(result);
         console.log(result);
       });
   };
@@ -22,6 +23,16 @@ const Home = () => {
     }
   };
 
+const renderResults = () => {
+    if(results && results.length ===0) {
+    return <div>No results</div>
+    }
+    if(results && results.length > 0){
+    return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+    }
+return null;
+}
+
   return (
     <MainPageLayout>
       <input
@@ -33,6 +44,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
