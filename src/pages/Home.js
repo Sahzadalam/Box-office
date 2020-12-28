4578a80267273e3aa13187e57710527dd58210c0
sahzad <sahjad.ranchi@gmail.com>
2020-12-28 11:40:04 +0530

    added small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import {apiGet} from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
 const [results, setResults] = useState(null);
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-          setResults(result);
-        console.log(result);
-      });
+
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onInputChange = ev => {
