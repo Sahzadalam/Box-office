605b503ae10bfe6f31eb19490c964c070506ce4b
sahzad <sahjad.ranchi@gmail.com>
2020-12-29 17:30:39 +0530

    styled the entire app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import ActorGrid from '../components/actor/ActorGrid';
+import CustomRadio from '../components/CustomRadio';
 import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import {apiGet} from '../misc/config';
 import { useLastQuery } from '../misc/custom-hooks';
+import { RadioInputsWrapper, SearchButtonWrapper, SearchInput } from './Home.styled';
 
 const Home = () => {
   const [input, setInput] = useLastQuery();
@@ -46,7 +48,7 @@ return null;
 
   return (
     <MainPageLayout>
-      <input
+      <SearchInput
         type="text"
         placeholder="Search for something"
         onChange={onInputChange}
@@ -54,22 +56,20 @@ return null;
         value={input}
       />
 
-      <div>
-
-      <label htmlFor="shows-search">
-        Shows
-        <input id ="shows-search" type="radio" value="shows" checked={isShowsSearch} onChange={onRadioChange}/>
-      </label>
-      <label htmlFor="actors-search">
-        Actors
-        <input id ="actors-search" type="radio" value="people" checked={!isShowsSearch} onChange={onRadioChange}/>
-      </label>
-
-
+      <RadioInputsWrapper>
+        <div>
+          <CustomRadio label="Shows" id ="shows-search" value="shows" checked={isShowsSearch} onChange={onRadioChange}/>
       </div>
+      <div>
+        <CustomRadio label="Actors" id ="actors-search" value="people" checked={!isShowsSearch} onChange={onRadioChange}/>
+      </div>
+
+      </RadioInputsWrapper>
+      <SearchButtonWrapper>
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      </SearchButtonWrapper>
       {renderResults()}
     </MainPageLayout>
   );
